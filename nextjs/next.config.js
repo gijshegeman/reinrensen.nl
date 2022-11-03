d5e1eab7c9513c12f1b996507c4b3b86123a497a
gijshegeman <gijshegeman@hotmail.com>
2022-11-03 10:15:26 +0100

    d

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'standalone',
+    reactStrictMode: true,
+    experimental: {
+        outputStandalone: true,
+    },
 }
 
 module.exports = nextConfig
