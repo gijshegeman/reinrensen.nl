28d87b9fe55814de7f8fa50489bd040e901db470
gijshegeman <gijshegeman@hotmail.com>
2022-11-03 11:52:06 +0100

    Fix: corrected: cant compile image/future/next

--- nextjs/components/imgPrevieuw.js
+++ nextjs/components/imgPrevieuw.js
@@ -1,4 +1,4 @@
-import Image from 'next/future/image'
+import Image from 'next/image'
 import { AnimatePresence, motion } from 'framer-motion'
 
 const BackDrop = ({ setImgPrevieuwActive }) => {
--- nextjs/pages/bio.js
+++ nextjs/pages/bio.js
@@ -1,4 +1,4 @@
-import Image from 'next/future/image'
+import Image from 'next/image'
 import Layout from '../components/layout'
 import profilePic from '../public/img/profile.png'
 
