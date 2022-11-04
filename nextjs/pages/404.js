783456dc8392fa13af228d7ed26ca98b13258a1f
gijshegeman <gijshegeman@hotmail.com>
2022-11-04 16:55:01 +0100

    Added layout

--- nextjs/pages/404.js
+++ nextjs/pages/404.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function error404() {
 
@@ -7,15 +8,18 @@ export default function error404() {
         <Head>
             <title>Rein Rensen | Error 404</title>
         </Head>
-        <div className="flex flex-col gap-10 items-center">
+        <Layout>
+            <div className="flex flex-col gap-10 items-center">
 
-            <div className="font-thin text-2xl">HTTP 404 | Page not found.</div>
+                <div className="font-thin text-2xl">HTTP 404 | Page not found.</div>
 
-            <div className="flex flex-col gap-2 items-center">
-                <div>Hey, de pagina die u probeert te bezoeken lijkt niet te bestaat!</div>
-                <Link href='/' className="text-[#21564e] hover:text-[#92aba6] animate ease-out duration-300">&larr; terug naar home</Link>
+                <div className="flex flex-col gap-2 items-center">
+                    <div>Hey, de pagina die u probeert te bezoeken lijkt niet te bestaat!</div>
+                    <Link href='/' className="text-[#21564e] hover:text-[#92aba6] animate ease-out duration-300">&larr; terug naar home</Link>
+                </div>
+
             </div>
+        </Layout>
 
-        </div>
     </>)
 }
