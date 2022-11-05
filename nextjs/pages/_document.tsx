0ab3e82f0f7de821bcad50c032b69839de7a7777
gijshegeman <gijshegeman@hotmail.com>
2022-11-05 14:23:11 +0100

    fix

--- nextjs/pages/_document.tsx
+++ nextjs/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='nl'> 
       <Head>
 
         <link rel="icon" href='/favicon.ico?=v3' />
