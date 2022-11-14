c64170e3309ce7190135e9035efade5c2a1f7424
gijshegeman <gijshegeman@hotmail.com>
2022-11-14 14:18:21 +0100

    Added force recreate on new container!

--- build.sh
+++ build.sh
@@ -72,24 +72,29 @@ zero_downtime_deploy() {
   old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
 
   # bring a new container online, running new code  
-  # (nginx continues routing to the old container only)  
-  docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
+  # (nginx continues routing to the old container only)
+  echo -e "${GREEN}bring a new container online, running new code, (nginx continues routing to the old container only)!${NOCOLOR}" 
+  docker-compose up -d --no-deps --scale $service_name=2 --force-recreate $service_name
 
-  # wait for new container to be available  
+  # wait for new container to be available
+  echo -e "${GREEN}Wait for new container to be available. ${NOCOLOR}"  
   new_container_id=$(docker ps -f name=$service_name -q | head -n1)
   new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
   curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
 
   # start routing requests to the new container (as well as the old)  
+  echo -e "${GREEN}Start routing requests to the new container (as well as the old). ${NOCOLOR}"  
   reload_nginx
 
   # take the old container offline  
+  echo -e "${GREEN}Take the old container offline. ${NOCOLOR}"  
   docker stop $old_container_id
   docker rm $old_container_id
 
   docker-compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
 
-  # stop routing requests to the old container  
+  # stop routing requests to the old container
+  echo -e "${GREEN}Stop routing requests to the old container. ${NOCOLOR}"   
   reload_nginx  
 }
 
