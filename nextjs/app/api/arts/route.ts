025ff416d4cf640e2739ed6210d8c142a6754a3b
gijshegeman <gijshegeman@hotmail.com>
2024-07-31 12:58:50 +0200

    API update

--- nextjs/app/api/arts/route.ts
+++ nextjs/app/api/arts/route.ts
@@ -3,27 +3,31 @@ import data from "./data.json";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  // Ensure proper type definition if needed
-  type ArtType = {
-    id: number;
-    volgorde: number;
-    title: string;
-    typeArt: string;
-    src: string;
-    y: number;
-    x: number;
-    price: string;
-    sizeCM: string;
-    dateMade: string;
-    dateAdded: string;
-    publish: boolean;
-    subtitle?: string;
-  };
+  try {
+    type ArtType = {
+      id: number;
+      volgorde: number;
+      title: string;
+      typeArt: string;
+      src: string;
+      y: number;
+      x: number;
+      price: string;
+      sizeCM: string;
+      dateMade: string;
+      dateAdded: string;
+      publish: boolean;
+      subtitle?: string;
+    };
 
-  const artsData: ArtType[] = data.arts as ArtType[];
+    const artsData: ArtType[] = data.arts as ArtType[];
 
-  // Filter the arts to get only the published ones
-  const arts: ArtType[] = artsData.filter((art: ArtType) => art.publish);
+    // Filter the arts to get only the published ones
+    const arts: ArtType[] = artsData.filter((art: ArtType) => art.publish);
 
-  return NextResponse.json(arts, { status: 200 });
+    return NextResponse.json(arts, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to read data file" });
+  }
 }
